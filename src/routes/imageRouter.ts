f436b24a11273f008def1e1dbc1ec19c96d8f7e9
pedrovitors <pedrovitorfhs@gmail.com>
2021-07-02 22:50:40 -0300

    Fixed Routes memes

--- src/routes/imageRouter.ts
+++ src/routes/imageRouter.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import {ImageController} from "../controller/ImageController";
 
 export const imageRouter = express.Router()
 const imageController = new ImageController
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-export const userRouter = express.Router
+export const userRouter = express.Router()
 const userController = new UserController
 
 userRouter.post("/signup", userController.signup)
